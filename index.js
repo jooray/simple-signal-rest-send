12d8bdc535bc69663b22fc7171d66741d8b0d40f
Juraj Bednar <juraj@bednar.io>
2020-07-07 00:19:39 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var port=6775;
+var bind_uri='/wheretobind1337/signal_send';
+var username='+123458718123';
+var default_recipient='+123989817292';
+var signal_cli_path='/usr/local/bin/signal-cli';
+
+
+const { spawnSync } = require('child_process');
+var express = require("express");
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => {
+ console.log("Server running on port "+port);
+ app.post(bind_uri, (req, res) => {
+  var recipient=default_recipient;
+  if (req.body.recipient) recipient=req.body.recipient;
+  console.log('Sending ' + req.body.message + ' to ', recipient);
+  const signal_cli = spawnSync(signal_cli_path, ['-u', username, 'send', recipient],
+      {
+        input: req.body.message,
+        timeout: 15000,
+        killSignal: "SIGKILL",
+        cwd: process.cwd(),
+        env: process.env,
+      });
+
+  return res.send({
+    error: signal_cli.error,
+    stdout: signal_cli.stdout.toString('utf8'),
+    stderr: signal_cli.stderr.toString('utf8')
+  });
+
+})
+});
